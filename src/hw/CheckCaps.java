1a1b285077880cec454b2b58a0d46a7b68b6eeb3
mdislam786 <abc@123.com>
2019-01-06 13:56:26 -0500

    just commited

---
+++ src/hw/CheckCaps.java
@@ -0,0 +1,12 @@
+package hw;
+
+public class CheckCaps {
+
+	public static void main(String[] args) {
+		
+		String str = "Hello, this is AP.  I am a teacher here. It's almost over. Just be patient.  ";
+		
+
+	}
+
+}
---
+++ src/sessionFour/LearnSplit.java
@@ -0,0 +1,16 @@
+package sessionFour;
+
+public class LearnSplit {
+
+	public static void main(String[] args) {
+			
+		
+		String items = "apple, orange,  grape fruit, pear, grapes, mango";
+		
+		String[] itemMultiple = items.split(",");
+		
+		System.out.println(itemMultiple[2]);
+		
+	}
+
+}
